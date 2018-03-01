709ee2edb5e15cfa96fbd171cbd98ebac72634a6
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-01 17:59:46 +0100

    stole a basic eye detector

---
+++ fetch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml > haarcascade_eye.xml
+curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml > haarcascade_frontalface_default.xml
